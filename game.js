3f98ca67e0b10bb336e60462d92c35b27430a3d0
PJC-1 <jumpzine@gmail.com>
2017-01-07 09:28:37 -0800

    added some comments for important methods

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 console.log("sanity check");
 
 var myCanvas = document.getElementById("myCanvas");
+// The HTMLCanvasElement.getContext() method returns a drawing context on the
+// canvas, or null if the context identifier is not supported.
+// "2d", leading to the creation of a CanvasRenderingContext2D object
+// representing a two-dimensional rendering context.
 var ctx = myCanvas.getContext("2d");
 
 var xPos = 60;
@@ -9,14 +13,19 @@ var width = 50;
 var height = 50;
 
 function drawFrame(){
-    // clearRect will clear the canvas
+    // The clearRect() method clears the specified pixels within a given rectangle.
+    // syntax: context.clearRect(x,y,width,height)
     ctx.clearRect(0,0,300,300)
+    // The fillStyle property sets or returns the color, gradient, or pattern
+    // used to fill the drawing.
     ctx.fillStyle="blue";
+    // The fillRect() method draws a "filled" rectangle. The default color of the fill is black.
+    // syntax: context.fillRect(x,y,width,height);
     ctx.fillRect(xPos, yPos, width, height)
     xPos++;
     yPos++;
 }
 
-// The setInterval() method calls a function or evaluates an expression at 
+// The setInterval() method calls a function or evaluates an expression at
 // specified intervals (in milliseconds).
 window.setInterval(drawFrame, 100)
