f74c4962c119dc8e1d80257563ddad4bf94e6b26
PJC-1 <jumpzine@gmail.com>
2017-01-07 09:21:41 -0800

    now box is moving

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
 console.log("sanity check");
+
+var myCanvas = document.getElementById("myCanvas");
+var ctx = myCanvas.getContext("2d");
+
+var xPos = 60;
+var yPos = 90;
+var width = 50;
+var height = 50;
+
+function drawFrame(){
+    // clearRect will clear the canvas
+    ctx.clearRect(0,0,300,300)
+    ctx.fillStyle="blue";
+    ctx.fillRect(xPos, yPos, width, height)
+    xPos++;
+    yPos++;
+}
+
+// The setInterval() method calls a function or evaluates an expression at 
+// specified intervals (in milliseconds).
+window.setInterval(drawFrame, 100)
