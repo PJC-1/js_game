785f885226f19314efa3506973e4cdd67f048c4a
PJC-1 <jumpzine@gmail.com>
2017-01-07 09:39:09 -0800

    can now move with the directional keys

--- game.js
+++ game.js
@@ -26,6 +26,29 @@ function drawFrame(){
     yPos++;
 }
 
+function moveBlock(event){
+    // left
+    if(event.keyCode === 37){
+        xPos -= 10;
+    }
+    // right
+    if(event.keyCode === 39){
+        xPos += 10;
+    }
+    // up
+    if(event.keyCode === 38){
+        yPos -= 10;
+    }
+    // down
+    if(event.keyCode === 40){
+        yPos += 10;
+    }
+}
+
 // The setInterval() method calls a function or evaluates an expression at
 // specified intervals (in milliseconds).
 window.setInterval(drawFrame, 100)
+
+// The document.addEventListener() method attaches an event handler to the document.
+// It takes to arguments, an event to listen for and a function to execute
+window.addEventListener("keydown", moveBlock)
